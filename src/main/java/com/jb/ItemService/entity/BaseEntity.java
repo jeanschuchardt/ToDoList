de31dbc3f6177eced4693f9dfb66c05d3e8dcbb4
Jean <jean@sherpaerp.com>
2023-05-08 20:33:37 -0300

    create class BaseEntity

---
+++ src/main/java/com/jb/ItemService/entity/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.jb.ItemService.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date dateCreated;
+    
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date dateUpdated;
+    
+    @Column
+    private Integer createdBy;
+    
+    @Column
+    private Integer updatedBy;
+    
+    @Column
+    private int isArchived;
+    
+    
+ 
+}
---
+++ src/main/java/com/jb/ItemService/entity/List.java
@@ -0,0 +1,15 @@
+package com.jb.ItemService.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class List extends BaseEntity {
+    
+    
+
+    
+    
+
+}
