1b4e71cee25a73294bbbfe90a2b364167a027afb
Jean <jean@sherpaerp.com>
2023-05-10 18:40:36 -0300

    renamed user class

--- src/main/java/com/jb/ItemService/entity/User.java
+++ src/main/java/com/jb/ItemService/entity/User.java
@@ -2,11 +2,14 @@ package com.jb.ItemService.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+
+import java.util.Set;
 
 @Entity
 @Data
-public class UserC extends BaseEntity {
+public class User extends BaseEntity {
     @Column
     private String name;
     @Column
@@ -16,10 +19,10 @@ public class UserC extends BaseEntity {
     @Column
     private String password;
     
-//    @OneToMany(mappedBy = "user")
-//    private Set<TaskList> taskList;
+    @OneToMany(mappedBy = "user")
+    private Set<TaskList> taskList;
     
-//    @OneToMany(mappedBy = "assignedUser")
-//    private Set<TaskItem> itemsAssigned;
+    @OneToMany(mappedBy = "assignedUser")
+    private Set<TaskItem> itemsAssigned;
     
 }
