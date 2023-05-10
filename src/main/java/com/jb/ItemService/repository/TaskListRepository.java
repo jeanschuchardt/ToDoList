a9c3308c9b71fb4d94ca4d93d40ca55103a42dcc
Jean <jean@sherpaerp.com>
2023-05-10 16:40:11 -0300

    create repository

---
+++ src/main/java/com/jb/ItemService/repository/TaskListRepository.java
@@ -0,0 +1,12 @@
+package com.jb.ItemService.repository;
+
+import com.jb.ItemService.entity.TaskItem;
+import com.jb.ItemService.entity.TaskList;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TaskListRepository extends BaseRepository<TaskList> {
+
+
+}
---
+++ src/main/java/com/jb/ItemService/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.jb.ItemService.repository;
+
+import com.jb.ItemService.entity.TaskList;
+import com.jb.ItemService.entity.User;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends BaseRepository<User> {
+
+
+}
