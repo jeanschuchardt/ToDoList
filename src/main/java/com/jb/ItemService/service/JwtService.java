f3bde1ca305f9ca44def68cedc01fcb5b4dd117e
Jean <jean@sherpaerp.com>
2023-05-12 16:55:04 -0300

    jwt

---
+++ src/main/java/com/jb/ItemService/service/JwtService.java
@@ -0,0 +1,15 @@
+package com.jb.ItemService.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    
+    
+    public String extractUsername(String token) {
+        
+        return null;
+        
+        
+    }
+}
